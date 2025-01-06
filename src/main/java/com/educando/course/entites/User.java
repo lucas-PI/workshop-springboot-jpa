200e248fe8690e723f2993c6b7b79b3ea664c885
lucas-PI <lucas22inacio@hotmail.com>
2025-01-06 09:55:43 -0300

    h2 database,test profile, JPA

--- src/main/java/com/educando/course/entites/User.java
+++ src/main/java/com/educando/course/entites/User.java
@@ -1,12 +1,18 @@
 package com.educando.course.entites;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb_users")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -18,6 +24,7 @@ public class User implements Serializable {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
+
         this.id = id;
         this.name = name;
         this.email = email;
