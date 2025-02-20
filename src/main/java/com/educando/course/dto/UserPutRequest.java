fae04a127569166ade1fef587cbcfa9dd9f9d337
lucas-PI <lucas22inacio@hotmail.com>
2025-02-20 12:31:22 -0300

    creating dtos classes for users

---
+++ src/main/java/com/educando/course/dto/UserPutRequest.java
@@ -0,0 +1,43 @@
+package com.educando.course.dto;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class UserPutRequest implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1l;
+    private String name;
+    private String email;
+    private String phone;
+
+    public UserPutRequest(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
