314d94da7790e827ed4ecba7aec3fcc683fc8656
lucas-PI <lucas22inacio@hotmail.com>
2025-03-14 09:07:02 -0300

    alteraçÃão no condigo sem acrecentar uma nova funcionalidade apenas na estrutra do codigo deixando mais legivel

--- src/main/java/com/educando/course/Controllers/execptions/ResourceExceptionHandler.java
+++ src/main/java/com/educando/course/Controllers/execptions/ResourceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.educando.course.Controllers.execptions;
 
 import com.educando.course.services.exception.DatabaseException;
+import com.educando.course.services.exception.ResourceByNameNotFound;
 import com.educando.course.services.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.hibernate.dialect.Database;
@@ -16,17 +17,53 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
-
+    /*
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
+     */
+        return new ResponseEntity<>(StandardError.builder()
+                .timestamp(Instant.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error("Resource not found")
+                .message(e.getMessage())
+                .path(request.getRequestURI())
+                .build(), HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> database(DatabaseException e,HttpServletRequest request){
+        /*
         String error = "Database Error";
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
+         */
+        return new ResponseEntity<>(StandardError.builder()
+                .timestamp(Instant.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error("Database Error")
+                .message(e.getMessage())
+                .path(request.getRequestURI())
+                .build(),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceByNameNotFound.class)
+    public ResponseEntity<StandardError> resourceByNameNotFound(ResourceByNameNotFound e, HttpServletRequest request){
+     /*  String error = "Resource by name not found";
+       HttpStatus status = HttpStatus.NOT_FOUND;
+       StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+       return ResponseEntity.status(status).body(err);
+
+      */
+        return new ResponseEntity<>(StandardError.builder()
+                .timestamp(Instant.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error("Resource by name not found")
+                .message(e.getMessage())
+                .path(request.getRequestURI()).build(),HttpStatus.NOT_FOUND);
     }
 }
+
+
+
